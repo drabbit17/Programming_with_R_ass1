3f6ce6029d210d04027e560d260671219d111148
drabbit17 <matteo.pallini17@gmail.com>
2015-03-18 15:08:00 +0100

    final assignment

---
+++ complete.R
@@ -0,0 +1,17 @@
+complete <- function(directory, id = 1:332 ){
+        wd <- "/Users/newaesthetic/data/R/coursera/programming_with_R/ass1"
+        setwd(paste(wd, directory, sep="/"))
+        n <- length(id)
+        dfrm <- data.frame(id=character(n), nobs=numeric(n))
+        for (i in id) {
+                ni <- sprintf("%03d", i)
+                temp <- read.csv(paste(ni,".csv", sep=""))
+                l <- match(i, id)
+                dfrm$id[l] <- i
+                counts <- is.na(temp$sulfate)
+                countn <- is.na(temp$nitrate)
+                dfrm$nobs[l] <- sum(as.numeric(!counts)*as.numeric(!countn))
+        }
+        setwd("/Users/newaesthetic/data/R/coursera/programming_with_R/ass1")
+        return(dfrm)
+}
---
+++ corr.R
@@ -0,0 +1,21 @@
+corr <- function(directory, threshold=0){
+        wd <- "/Users/newaesthetic/data/R/coursera/programming_with_R/ass1"
+        setwd(wd)
+        source("complete.R")
+        dfrm <- complete(directory)
+        setwd(paste(wd, directory, sep="/"))
+        valid <- numeric(length(dfrm[[1]]))
+        valid <- ifelse (dfrm$nobs>threshold,valid <- 1,valid <- 0) 
+        dfrm <- cbind(dfrm, valid)
+        use <- subset(dfrm, select=id, subset=(dfrm$valid>0))
+        use <- as.numeric(use[[1]])
+        corvect <- numeric(length(use))
+        for (i in use){
+                ni <- sprintf("%03d", as.numeric(i))
+                temp <- read.csv(paste(ni,".csv", sep=""))
+                l <- match(i, use)
+                corvect[l] <- cor(temp$sulfate, temp$nitrate, use="na.or.complete")
+        }
+        setwd("/Users/newaesthetic/data/R/coursera/programming_with_R/ass1")
+        return(corvect)       
+}
