2a48605b5faec59ad2ac0734ab2704bd80617989
drabbit17 <matteo.pallini17@gmail.com>
2015-05-21 19:36:02 +0200

    Update complete.R

--- complete.R
+++ complete.R
@@ -1,11 +1,11 @@
 complete <- function(directory, id = 1:332 ){
         wd <- "/Users/newaesthetic/data/R/coursera/programming_with_R/ass1"
-        setwd(paste(wd, directory, sep="/"))
+        setwd(paste(wd, directory, sep = "/"))
         n <- length(id)
-        dfrm <- data.frame(id=character(n), nobs=numeric(n))
+        dfrm <- data.frame(id=character(n), nobs = numeric(n))
         for (i in id) {
                 ni <- sprintf("%03d", i)
-                temp <- read.csv(paste(ni,".csv", sep=""))
+                temp <- read.csv(paste(ni,".csv", sep = ""))
                 l <- match(i, id)
                 dfrm$id[l] <- i
                 counts <- is.na(temp$sulfate)
