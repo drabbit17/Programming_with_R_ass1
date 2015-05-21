46dc143e70862952b037e25aa54a91258462cc6d
drabbit17 <matteo.pallini17@gmail.com>
2015-05-21 19:37:48 +0200

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,16 +1,16 @@
 pollutantmean <- function(directory, pollutant, id = 1:332 ){
         wd <- "/Users/newaesthetic/data/R/coursera/programming_with_R/ass1"
-        setwd(paste(wd, directory, sep="/"))
+        setwd(paste(wd, directory, sep = "/"))
         n <- length(id)
         vmean <- numeric(n)
         lmean <- numeric(n)
         for (i in id) {
                 ni <- sprintf("%03d", i)
-                temp <- read.csv(paste(ni,".csv", sep=""))
+                temp <- read.csv(paste(ni,".csv", sep = ""))
                 l <- match(i, id)
                 x <- temp[pollutant]
-                reallength <- length(x[[1]])-sum(is.na(x[[1]]))
-                vmean[l] <- mean(x[[1]], na.rm = TRUE)*reallength
+                reallength <- length(x[[1]]) - sum(is.na(x[[1]]))
+                vmean[l] <- mean(x[[1]], na.rm = TRUE) * reallength
                 lmean[l] <- reallength
         }
         setwd("/Users/newaesthetic/data/R/coursera/programming_with_R/ass1")
